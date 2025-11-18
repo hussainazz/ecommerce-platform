3361deeaafb568d46f8b12e5bb5a7beb7dd4e436
hussainazz <hosseinazizi267@gmail.com>
2025-11-18 19:30:53 +0330

    prevent multiple collectoin creation

--- src/db/schemas/order.schema.ts
+++ src/db/schemas/order.schema.ts
@@ -1,65 +1,75 @@
-import { database } from "@config/database.ts";
+import { database } from "../database.ts";
 
-export const orderCollection = await database.createCollection("Order", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "Order Validation",
-      required: ["user_id", "items", "totalPrice", "status"],
-      properties: {
-        user_id: {
-          bsonType: "objectId",
-          description: "Reference to the User who placed the order",
-        },
-        products: {
-          bsonType: "array",
-          description: "List of products and quantities",
-          minItems: 1,
-          items: {
-            bsonType: "object",
-            required: ["product_id", "count"],
-            properties: {
-              product_id: {
-                bsonType: "objectId",
-                description: "Reference to the Product",
-              },
-              count: {
-                bsonType: "int",
-                minimum: 1,
-                description: "Quantity must be at least 1",
+export let orderCollection = database.collection("Order");
+if (!orderCollection) {
+  orderCollection = await database.createCollection("Order", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Order Validation",
+        required: ["user_id", "items", "totalPrice", "status"],
+        properties: {
+          user_id: {
+            bsonType: "objectId",
+            description: "Reference to the User who placed the order",
+          },
+          products: {
+            bsonType: "array",
+            description: "List of products and quantities",
+            minItems: 1,
+            items: {
+              bsonType: "object",
+              required: ["product_id", "count"],
+              properties: {
+                product_id: {
+                  bsonType: "objectId",
+                  description: "Reference to the Product",
+                },
+                count: {
+                  bsonType: "int",
+                  minimum: 1,
+                  description: "Quantity must be at least 1",
+                },
               },
             },
           },
-        },
-        totalPrice: {
-          bsonType: "long",
-          minimum: 0,
-          description: "Total cost of the order",
-        },
-        shipping_address: {
-          bsonType: "object",
-          description: "Optional shipping details",
-          required: ["street", "city", "province", "postCode"],
-          properties: {
-            street: { bsonType: "string" },
-            city: { bsonType: "string" },
-            province: { bsonType: "string" },
-            postCode: { bsonType: "int" },
+          totalPrice: {
+            bsonType: "long",
+            minimum: 0,
+            description: "Total cost of the order",
           },
+          shipping_address: {
+            bsonType: "object",
+            description: "Optional shipping details",
+            required: ["street", "city", "province", "postCode"],
+            properties: {
+              street: { bsonType: "string" },
+              city: { bsonType: "string" },
+              province: { bsonType: "string" },
+              postCode: { bsonType: "int", length: 10 },
+            },
+          },
+          status: {
+            bsonType: "string",
+            enum: ["completed", "confirmed", "pending", "canceled"],
+            default: "pending",
+            description: "Current status of the order",
+          },
+          created_at: { bsonType: "number" },
+          confirmed_at: { bsonType: "number" },
+          completed_at: { bsonType: "number" },
+          canceled_at: { bsonType: "number" },
         },
-        status: {
-          bsonType: "string",
-          enum: ["completed", "confirmed", "pending", "canceled"],
-          default: "pending",
-          description: "Current status of the order",
-        },
-        created_at: { bsonType: "number" },
-        confirmed_at: { bsonType: "number" },
-        completed_at: { bsonType: "number" },
-        canceled_at: { bsonType: "number" },
       },
+      // ensure postcode is 10-digits
+      $expr: {
+        $and: [
+          { $gte: ["$postCode", 1000000000] },
+          { $lt: ["$postCode", 10000000000] },
+        ],
+      },
+      validationLevel: "strict",
+      validationAction: "error",
     },
-    validationLevel: "strict",
-    validationAction: "error",
-  },
-});
+  });
+}
--- src/db/schemas/user.schema.ts
+++ src/db/schemas/user.schema.ts
@@ -1,35 +1,39 @@
-import { database } from "@config/database.ts";
-export const userCollection = await database.createCollection("User", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "User Validation",
-      required: ["username", "password", "email", "role"],
-      properties: {
-        email: {
-          unique: true,
-          bsonType: "string",
-          pattern: "^[^@]+@[^@]+\\.[^@]+$",
-          description: "email must be a string and is required",
-        },
-        username: {
-          bsonType: "string",
-          description: "username must be a string and is required",
-        },
-        password: {
-          bsonType: "string",
-          description: "password must be a string and is required",
-        },
-        role: {
-          bsonType: "string",
-          description: "role must be a string and is required",
+import { database } from "../database.ts";
+
+export let userCollection = database.collection("User");
+if (!userCollection) {
+  userCollection = await database.createCollection("User", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "User Validation",
+        required: ["username", "password", "email", "role"],
+        properties: {
+          email: {
+            unique: true,
+            bsonType: "string",
+            pattern: "^[^@]+@[^@]+\\.[^@]+$",
+            description: "email must be a string and is required",
+          },
+          username: {
+            bsonType: "string",
+            description: "username must be a string and is required",
+          },
+          password: {
+            bsonType: "string",
+            description: "password must be a string and is required",
+          },
+          role: {
+            bsonType: "string",
+            description: "role must be a string and is required",
+          },
         },
       },
+      validationLevel: "strict",
+      validationAction: "error",
     },
-    validationLevel: "strict",
-    validationAction: "error",
-  },
-});
+  });
+}
 
 await userCollection.createIndex({ email: 1 }, { unique: true });
 await userCollection.createIndex({ username: 1 }, { unique: true });
