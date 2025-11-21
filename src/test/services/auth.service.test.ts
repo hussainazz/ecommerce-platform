5013a43afc6bbb9d0913ba088d625d3157c4a3e6
hussainazz <hosseinazizi267@gmail.com>
2025-11-21 22:35:59 +0330

    base token service-level handler

--- src/test/services/auth.service.test.ts
+++ src/test/services/auth.service.test.ts
@@ -1,18 +1,22 @@
 import { userCollection } from "@db/schemas/user.schema.ts";
 import { UserService } from "@features/auth/auth.service.ts";
-import { ConnectionCheckOutStartedEvent, ObjectId } from "mongodb";
+import { hashPassword, verifyPassword } from "@features/auth/auth.service.ts";
+import { ObjectId } from "mongodb";
 
-let testID: any;
+let testID: string | ObjectId;
+let testID1: ObjectId;
+let refreshTokenRaw = "asldhgklha2350802lsd";
+let refTokenMaxAge = 15 * 24 * 60 * 60 * 1000;
 
 beforeAll(async () => {
   await userCollection.deleteMany({});
   const testUser = await userCollection.insertOne({
     username: "test username",
-    password: 123456789,
+    password: await hashPassword("123456789"),
     email: "test@test.com",
     role: "user",
   });
-  testID = testUser.insertedId.toString();
+  testID = testUser.insertedId;
 });
 
 afterAll(async () => {
@@ -21,18 +25,22 @@ afterAll(async () => {
 
 describe("UserService - integrationTest", () => {
   it("should create a user", async () => {
-    const user = await UserService.create({
-      username: "john",
-      password: "johny123456",
-      email: "john@johny.com",
-      role: "user",
-    });
+    const user = await UserService.register(
+      {
+        username: "john",
+        password: "johny123456",
+        email: "john@johny.com",
+      },
+      refreshTokenRaw,
+      refTokenMaxAge,
+    );
     const findUser = await userCollection.findOne({ username: user.username });
     expect(findUser?._id).toBeDefined();
+    testID1 = findUser?._id!;
   });
 
   it("should find user by id", async () => {
-    const user = await UserService.findById(testID);
+    const user = await UserService.findById(testID.toString());
     expect(user._id).toBeDefined;
   });
 
@@ -41,9 +49,42 @@ describe("UserService - integrationTest", () => {
     expect(userId).toBeDefined();
   });
 
+  it("should created refresh token on register", async () => {
+    const user = await userCollection.findOne({ _id: testID1 });
+    console.log(JSON.stringify(user, null, 2));
+    expect(user?.refreshTokens[0].tokenHash).toBeDefined();
+  });
+
+  it("should push refresh token", async () => {
+    await UserService.storeToken(testID1, refreshTokenRaw, refTokenMaxAge);
+    const updatedUser = await userCollection.findOne({
+      _id: testID1,
+    });
+    expect(updatedUser?.refreshTokens[1].tokenHash).toBeDefined();
+  });
+
   it("should update password", async () => {
-    const updateResult = await UserService.updatePassword(testID, "98765432");
+    const updateResult = await UserService.updatePassword(
+      testID.toString(),
+      "98765432",
+    );
     expect(updateResult.modifiedCount).toEqual(1);
+  });
+
+  it("should be truthy when password matches", async () => {
+    const result = await UserService.isPassMatch(
+      testID.toString(),
+      "123456789",
+    );
+    expect(result).toBeTruthy;
+  });
+
+  it("should be falsy when password not matches", async () => {
+    const result = await UserService.isPassMatch(
+      testID.toString(),
+      "987654321",
+    );
+    expect(result).toBeFalsy;
   });
 
   it("should throw when updating password non-existent user", async () => {
@@ -71,7 +112,7 @@ describe("UserService - integrationTest", () => {
   });
 
   it("should delete user", async () => {
-    const deletedUser = await UserService.delete(testID);
+    const deletedUser = await UserService.delete(testID.toString());
     expect(deletedUser.deletedCount).toEqual(1);
   });
 });
