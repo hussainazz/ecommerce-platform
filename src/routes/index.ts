916779f9dc21438c10761c088aad12599dc06b3f
hussainazz <hosseinazizi267@gmail.com>
2025-11-27 21:00:10 +0330

    add "/users" route handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { router as authRouter } from "@features/auth/auth.controller.ts";
 import { router as productRouter } from "@features/products/product.controller.ts";
+import { authMiddleware } from "@shared/middlewares/auth.middleware.ts";
+import { router as userRouter } from "@features/users/users.router.ts";
 
 export const routerV1 = express.Router();
 
-routerV1.all("/auth", authRouter);
-routerV1.all("/products", productRouter);
+routerV1.use("/auth", authRouter);
+routerV1.use("/products", productRouter);
+routerV1.use("/users", authMiddleware, userRouter);
